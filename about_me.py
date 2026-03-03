231a3bc78f698cafaded817baf4e0ab264a7449d
kathiravanj583224106051 <kathiravanj583224106051@nprcolleges.org>
2026-03-03 17:28:29 +0530

    Create about_me.py

---
+++ about_me.py
@@ -0,0 +1,5 @@
+print("Hello! 👋")
+print("My name is Kathiravan.")
+print("This is my first GitHub repository.")
+print("I am learning Python programming.")
+print("I am excited to explore coding and new technologies! 🚀
